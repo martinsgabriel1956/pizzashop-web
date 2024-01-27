a84a5064e8d3f57c52e77992630d96bc2efeee03
Gabriel Martins <martinsgabriel1956@gmail.com>
2024-01-27 13:08:58 -0300

    chore: install and setting shadcn/ui lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import '@/global.css';
+import { Button } from './components/ui/button';
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="flex justify-center items-center h-screen">
+      <Button>
+        Enviar
+      </Button>
+    </div>
   )
 }
