e9c18e087188ac26632f535d504b93f6dc0b7eb7
Gabriel Martins <martinsgabriel1956@gmail.com>
2024-01-30 16:56:49 -0300

    feat: create the sign-in and auth layout structures and styles them

--- src/view/pages/auth/sign-in/index.tsx
+++ src/view/pages/auth/sign-in/index.tsx
@@ -1,10 +1,33 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/view/components/ui/button'
+import { Input } from '@/view/components/ui/input'
+import { Label } from '@/view/components/ui/label'
+
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>SignIn</h1>
+      <div className="flex w-[350px] flex-col justify-center gap-6">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Acessar painel
+          </h1>
+          <p className="text-muted-foreign text-sm">
+            Acompanhe suas venda pelo painel do parceiro!
+          </p>
+        </div>
+        <form className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Seu e-mail</Label>
+            <Input id="email" type="email" />
+          </div>
+
+          <Button className="w-full" type="submit">
+            Acessar painel
+          </Button>
+        </form>
+      </div>
     </>
   )
 }
