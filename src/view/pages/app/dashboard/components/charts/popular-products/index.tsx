cfcc5ad3e9b67f4912882024c655d6672196a254
Gabriel Martins <martinsgabriel1956@gmail.com>
2024-01-31 12:24:22 -0300

    feat: create popular products chart component

---
+++ src/view/pages/app/dashboard/components/charts/popular-products/index.tsx
@@ -0,0 +1,97 @@
+import { BarChart } from 'lucide-react'
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/view/components/ui/card'
+
+const MOCKED_DATA = [
+  { product: 'Pepperoni', amount: 40 },
+  { product: 'Portuguesa', amount: 30 },
+  { product: '4 Queijos', amount: 50 },
+  { product: 'Pepperoni', amount: 16 },
+  { product: 'Mussarela', amount: 26 },
+]
+
+const COLORS = [
+  colors.sky[500],
+  colors.amber[500],
+  colors.violet[500],
+  colors.emerald[500],
+  colors.rose[500],
+]
+
+export function PopularProductsChart() {
+  return (
+    <Card className="col-span-3">
+      <CardHeader className="pb-8">
+        <div className="flex items-center justify-between">
+          <CardTitle className="text-base font-medium">
+            Produtos populares
+          </CardTitle>
+          <BarChart className="h-4 w-4 text-muted-foreground" />
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <PieChart style={{ fontSize: 12 }}>
+            <Pie
+              data={MOCKED_DATA}
+              dataKey="amount"
+              nameKey="product"
+              cx="50%"
+              cy="50%"
+              outerRadius={86}
+              innerRadius={64}
+              strokeWidth={8}
+              labelLine={false}
+              label={({
+                cx,
+                cy,
+                midAngle,
+                innerRadius,
+                outerRadius,
+                value,
+                index,
+              }) => {
+                const RADIAN = Math.PI / 180
+                const radius = 12 + innerRadius + (outerRadius - innerRadius)
+                const x = cx + radius * Math.cos(-midAngle * RADIAN)
+                const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+                return (
+                  <text
+                    x={x}
+                    y={y}
+                    className="fill-muted-foreground text-xs"
+                    textAnchor={x > cx ? 'start' : 'end'}
+                    dominantBaseline="central"
+                  >
+                    {MOCKED_DATA[index].product.length > 12
+                      ? MOCKED_DATA[index].product
+                        .substring(0, 12)
+                        .concat('...')
+                      : MOCKED_DATA[index].product}{' '}
+                    ({value})
+                  </text>
+                )
+              }}
+            >
+              {MOCKED_DATA.map((_, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index]}
+                  className="stroke-background hover:opacity-80"
+                />
+              ))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
--- src/view/pages/app/dashboard/index.tsx
+++ src/view/pages/app/dashboard/index.tsx
@@ -1,12 +1,20 @@
 import { Helmet } from 'react-helmet-async'
-import { DayOrderAmountCard, MonthCanceledOrdersAmountCard, MonthOrdersAmountCard, MonthRevenueCard, RevenueChart } from './components'
+
+import {
+  DayOrderAmountCard,
+  MonthCanceledOrdersAmountCard,
+  MonthOrdersAmountCard,
+  MonthRevenueCard,
+  PopularProductsChart,
+  RevenueChart,
+} from './components'
 
 export function Dashboard() {
   return (
     <>
       <Helmet title="Dashboard" />
       <div className="flex flex-col gap-4 ">
-        <h1 className='text-3xl font-bold tracking-tight'>Dashboard</h1>
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
 
         <div className="grid grid-cols-4 gap-4">
           <MonthRevenueCard />
@@ -17,6 +25,7 @@ export function Dashboard() {
 
         <div className="grid grid-cols-9 gap-4">
           <RevenueChart />
+          <PopularProductsChart />
         </div>
       </div>
     </>
